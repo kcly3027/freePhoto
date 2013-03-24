6b16982bb3272518c8a98641f21c3373b0f8718c
kcly3027 <lc_g@163.com>
2013-03-24 22:43:53 +0800

    完成上传和裁减图片
    计划明天完成订单

--- Web/upimg.ashx.cs
+++ Web/upimg.ashx.cs
@@ -29,6 +29,10 @@ namespace freePhoto.Web
         /// 5 寸 5x3.5 12.7*8.9 1200x840以上 100万像素
         /// </summary>
         private Img CropImg = new Img() { Width = 1200, Height = 840 };
+        /// <summary>
+        /// 裁剪大小
+        /// </summary>
+        private Img CropZoom = new Img() { Width = 430, Height = 301 };
 
         private string fileTypes = "jpg,jpeg,png";
 
@@ -46,6 +50,9 @@ namespace freePhoto.Web
                 {
                     case "crop":
                         CropImage(context);
+                        break;
+                    case "cropzoom":
+                        CropZoomImage(context);
                         break;
                     case "upimg":
                         UpImage(context);
@@ -78,11 +85,26 @@ namespace freePhoto.Web
                 string fullpath = upimgs + savepath;
                 if (!Directory.Exists(upimgs)) Directory.CreateDirectory(upimgs);
                 if (File.Exists(fullpath) == false) file.SaveAs(fullpath);
+
                 Bitmap sourceBmp = new Bitmap(fullpath);
+                int width = sourceBmp.Width, height = sourceBmp.Height;
+                if (width > ZoomImg.Width || height > ZoomImg.Height)
+                {
+                    if (width > height)
+                    {
+                        width = (int)ZoomImg.Width;
+                        height = Convert.ToInt32((Convert.ToDouble(width) / Convert.ToDouble(sourceBmp.Width)) * height);
+                    }
+                    else
+                    {
+                        width = Convert.ToInt32((Convert.ToDouble(height) / Convert.ToDouble(sourceBmp.Height)) * width);
+                        height = (int)ZoomImg.Height;
+                    }
+                }
                 Image zoomPhoto = Image.FromStream(ImageClass.ResizeImage(
                     sourceBmp,
-                    Convert.ToInt32(ZoomImg.Width),
-                    Convert.ToInt32(ZoomImg.Height)
+                    Convert.ToInt32(width),
+                    Convert.ToInt32(height)
                 ));
                 if (!Directory.Exists(zoomurl)) Directory.CreateDirectory(zoomurl);
                 zoomPhoto.Save(zoomurl + imagekey + fileExt);
@@ -101,32 +123,31 @@ namespace freePhoto.Web
         /// <summary>
         /// 裁剪图片
         /// </summary>
-        private void CropImage(HttpContext context)
+        private void CropZoomImage(HttpContext context)
         {
             bool Check = false;
-            float viewPortW = GetFloat("viewPortW", out Check); if (!Check) goto CheckFaill;
-            float viewPortH = GetFloat("viewPortH", out Check); if (!Check) goto CheckFaill;
-            float imageX = GetFloat("imageX", out Check); if (!Check) goto CheckFaill;
-            float imageY = GetFloat("imageY", out Check); if (!Check) goto CheckFaill;
+            float scale = 2;
+
+            float viewPortW = GetFloat("viewPortW", out Check) * scale; if (!Check) goto CheckFaill;
+            float viewPortH = GetFloat("viewPortH", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageX = GetFloat("imageX", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageY = GetFloat("imageY", out Check) * scale; if (!Check) goto CheckFaill;
             float imageRotate = GetFloat("imageRotate", out Check); if (!Check) goto CheckFaill;
-            float imageW = GetFloat("imageW", out Check); if (!Check) goto CheckFaill;
-            float imageH = GetFloat("imageH", out Check); if (!Check) goto CheckFaill;
-            float selectorX = GetFloat("selectorX", out Check); if (!Check) goto CheckFaill;
-            float selectorY = GetFloat("selectorY", out Check); if (!Check) goto CheckFaill;
-            float selectorW = GetFloat("selectorW", out Check); if (!Check) goto CheckFaill;
-            float selectorH = GetFloat("selectorH", out Check); if (!Check) goto CheckFaill;
+            float imageW = GetFloat("imageW", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageH = GetFloat("imageH", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorX = GetFloat("selectorX", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorY = GetFloat("selectorY", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorW = GetFloat("selectorW", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorH = GetFloat("selectorH", out Check) * scale; if (!Check) goto CheckFaill;
             string imageSource = Request["imageSource"]; if (string.IsNullOrEmpty(imageSource)) goto CheckFaill;
             string ImgKey = Request["ImgKey"]; if (string.IsNullOrEmpty(imageSource)) goto CheckFaill;
 
-            imageW = (float)CropImg.Width;
-            imageH = (float)CropImg.Height;
-            selectorW = (float)CropImg.Width;
-            selectorH = (float)CropImg.Height;
             //To Values
             float pWidth = imageW;
             float pHeight = imageH;
 
-            Bitmap img = (Bitmap)Bitmap.FromFile(context.Server.MapPath("/upimages/original/" + ImgKey + ".jpg"));
+            //Bitmap img = (Bitmap)Bitmap.FromFile(context.Server.MapPath("/upimages/original/" + ImgKey + ".jpg"));
+            Bitmap img = (Bitmap)Bitmap.FromFile(context.Server.MapPath(imageSource));
             //Original Values
             int _width = img.Width;
             int _height = img.Height;
@@ -139,7 +160,7 @@ namespace freePhoto.Web
             //Rotate if angle is not 0.00 or 360
             if (imageRotate > 0.0F && imageRotate < 360.00F)
             {
-                image_p = (Bitmap)RotateImage(image_p, (double)imageRotate);
+                image_p = (Bitmap)RotateImg(image_p, (double)imageRotate);
                 pWidth = image_p.Width;
                 pHeight = image_p.Height;
             }
@@ -177,14 +198,105 @@ namespace freePhoto.Web
             //image_p.Save(context.Server.MapPath("test_viewport.jpg"));
             //Get Selector Portion
             image_p = ImageCopy(image_p, 0, 0, selectorX, selectorY, selectorW, selectorH, viewPortW, viewPortH);
-            string FileName = String.Format("test{0}.jpg", DateTime.Now.Ticks.ToString());
-            image_p.Save(context.Server.MapPath(FileName));
+            image_p.Save(context.Server.MapPath("/upimages/showcrop/" + ImgKey + ".jpg"));
 
 
             image_p.Dispose();
             img.Dispose();
             //imgSelector.Dispose();
-            OutPut(true, FileName);
+            OutPut(true, ImgKey);
+            return;
+
+        CheckFaill:
+            OutPut("Fail");
+            return;
+        }
+
+        /// <summary>
+        /// 裁剪图片
+        /// </summary>
+        private void CropImage(HttpContext context)
+        {
+            bool Check = false;
+            float scale = (float)(CropImg.Width / CropZoom.Width);
+
+            float viewPortW = GetFloat("viewPortW", out Check) * scale; if (!Check) goto CheckFaill;
+            float viewPortH = GetFloat("viewPortH", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageX = GetFloat("imageX", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageY = GetFloat("imageY", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageRotate = GetFloat("imageRotate", out Check); if (!Check) goto CheckFaill;
+            float imageW = GetFloat("imageW", out Check) * scale; if (!Check) goto CheckFaill;
+            float imageH = GetFloat("imageH", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorX = GetFloat("selectorX", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorY = GetFloat("selectorY", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorW = GetFloat("selectorW", out Check) * scale; if (!Check) goto CheckFaill;
+            float selectorH = GetFloat("selectorH", out Check) * scale; if (!Check) goto CheckFaill;
+            string imageSource = Request["imageSource"]; if (string.IsNullOrEmpty(imageSource)) goto CheckFaill;
+            string ImgKey = Request["ImgKey"]; if (string.IsNullOrEmpty(imageSource)) goto CheckFaill;
+
+            //To Values
+            float pWidth = imageW;
+            float pHeight = imageH;
+
+            Bitmap img = (Bitmap)Bitmap.FromFile(context.Server.MapPath("/upimages/original/" + ImgKey + ".jpg"));
+            //Bitmap img = (Bitmap)Bitmap.FromFile(context.Server.MapPath(imageSource));
+            //Original Values
+            int _width = img.Width;
+            int _height = img.Height;
+
+            //Resize
+            Bitmap image_p = ImageClass.ResizeBitmap(img, Convert.ToInt32(pWidth), Convert.ToInt32(pHeight));
+
+            int widthR = image_p.Width;
+            int heightR = image_p.Height;
+            //Rotate if angle is not 0.00 or 360
+            if (imageRotate > 0.0F && imageRotate < 360.00F)
+            {
+                image_p = (Bitmap)RotateImg(image_p, (double)imageRotate);
+                pWidth = image_p.Width;
+                pHeight = image_p.Height;
+            }
+
+            //Calculate Coords of the Image into the ViewPort
+            float src_x = 0;
+            float dst_x = 0;
+            float src_y = 0;
+            float dst_y = 0;
+
+            if (pWidth > viewPortW)
+            {
+                src_x = (float)Math.Abs(imageX - Math.Abs((imageW - pWidth) / 2));
+                dst_x = 0;
+            }
+            else
+            {
+                src_x = 0;
+                dst_x = (float)(imageX + ((imageW - pWidth) / 2));
+            }
+            if (pHeight > viewPortH)
+            {
+                src_y = (float)Math.Abs(imageY - Math.Abs((imageH - pHeight) / 2));
+                dst_y = 0;
+            }
+            else
+            {
+                src_y = 0;
+                dst_y = (float)(imageY + ((imageH - pHeight) / 2));
+            }
+
+
+            //Get Image viewed into the ViewPort
+            image_p = ImageCopy(image_p, dst_x, dst_y, src_x, src_y, viewPortW, viewPortH, pWidth, pHeight);
+            //image_p.Save(context.Server.MapPath("test_viewport.jpg"));
+            //Get Selector Portion
+            image_p = ImageCopy(image_p, 0, 0, selectorX, selectorY, selectorW, selectorH, viewPortW, viewPortH);
+            image_p.Save(context.Server.MapPath("/upimages/crop/" + ImgKey + ".jpg"));
+
+
+            image_p.Dispose();
+            img.Dispose();
+            //imgSelector.Dispose();
+            OutPut(true, ImgKey);
             return;
 
             CheckFaill:
@@ -248,7 +360,7 @@ namespace freePhoto.Web
 
         }
 
-
+        /*
         private Bitmap ResizeImage(Bitmap img, int width, int height)
         {
             Image.GetThumbnailImageAbort callback = new Image.GetThumbnailImageAbort(GetThumbAbort);
@@ -299,6 +411,108 @@ namespace freePhoto.Web
 
             //return the image
             return bmp;
+        }
+        */
+
+        /// <summary>
+
+        /// 以逆时针为方向对图像进行旋转
+
+        /// </summary>
+
+        /// <param name="b">位图流</param>
+
+        /// <param name="angle">旋转角度[0,360](前台给的)</param>
+
+        /// <returns></returns>
+
+        public Image RotateImg(Bitmap b, double angle)
+        {
+
+            angle = 0 - angle % 360;
+
+
+
+            //弧度转换
+
+            double radian = angle * Math.PI / 180.0;
+
+            double cos = Math.Cos(radian);
+
+            double sin = Math.Sin(radian);
+
+
+
+            //原图的宽和高
+
+            int w = b.Width;
+
+            int h = b.Height;
+
+            int W = (int)(Math.Max(Math.Abs(w * cos - h * sin), Math.Abs(w * cos + h * sin)));
+
+            int H = (int)(Math.Max(Math.Abs(w * sin - h * cos), Math.Abs(w * sin + h * cos)));
+
+
+
+            //目标位图
+
+            Bitmap dsImage = new Bitmap(W, H);
+
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(dsImage);
+
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bilinear;
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+
+
+            //计算偏移量
+
+            Point Offset = new Point((W - w) / 2, (H - h) / 2);
+
+
+
+            //构造图像显示区域：让图像的中心与窗口的中心点一致
+
+            Rectangle rect = new Rectangle(Offset.X, Offset.Y, w, h);
+
+            Point center = new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+
+
+
+            g.TranslateTransform(center.X, center.Y);
+
+            g.RotateTransform((float)(360 - angle));
+
+
+
+            //恢复图像在水平和垂直方向的平移
+
+            g.TranslateTransform(-center.X, -center.Y);
+
+            g.DrawImage(b, rect);
+
+
+
+            //重至绘图的所有变换
+
+            g.ResetTransform();
+
+
+
+            g.Save();
+
+            g.Dispose();
+
+            //保存旋转后的图片
+
+            b.Dispose();
+
+            //dsImage.Save("FocusPoint.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            return dsImage;
+
         }
 
         #endregion
