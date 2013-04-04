88631a286e4ec77e32c256c99097368e30866dc1
kcly3027 <lc_g@163.com>
2013-04-04 22:41:43 +0800

    需求更新

---
+++ Web/BaseSite.Master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace freePhoto.Web
+{
+    public partial class BaseSite : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
